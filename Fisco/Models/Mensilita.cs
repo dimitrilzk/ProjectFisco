a6204d00ebab8635d38d7ae6fbdff679b740f757
dimitrilzk <dimitrilazuka95@gmail.com>
2024-05-04 10:03:51 +0200

    Aggiungere i file di progetto.

---
+++ Fisco/Models/Mensilita.cs
@@ -0,0 +1,19 @@
+﻿namespace Fisco.Models
+{
+    public class Mensilita
+    {
+        public Guid Id { get; set; }
+        public decimal Stipendio { get; set; }
+        public decimal EntrateExtra { get; set; }
+        public string Risparmio { get; set; }
+        public string ContoComune { get; set; }
+        public string ContoWeekend { get; set; }
+        public decimal SpFisseMese { get; set; }
+        public decimal SpFisseAnno { get; set; }
+        public decimal UsciteExtra { get; set; }
+        public decimal RimanenzaMese { get; set; }
+
+        public string Mese { get; set; }
+        public string Anno { get; set; }
+    }
+}
