e758e68d41b3fc79bf246eb33696c83ff24986d2
Dimitri <dimitri.lazuka@e-projectsrl.it>
2024-07-30 23:02:51 +0200

    add js

---
+++ Fisco/wwwroot/js/Home.js
@@ -0,0 +1,12 @@
+﻿function askYear() {
+    $(".alreadyUser").addClass("d-none");
+    $(".new").addClass("d-none");
+    $('.yearForm').removeClass('d-none');
+}
+
+function takeYear() {
+    let input = $('#year').val();
+    console.log(input);
+    $('.yearForm').addClass('d-none');
+    alert('Generazione tabella in corso...');
+}
